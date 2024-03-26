795a25a29ab8a841d5d79995ab64de71de4b7b1d
yzm <13884935@qq.com>
2024-03-26 19:42:19 +0900

    2

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -66,7 +66,7 @@ export default function page() {
           type="file"
           name="file"
           className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          placeholder="0.00"
+         
           onChange={(e) => setFile(e.target.files?.[0])}
           ref={ref}
         />
