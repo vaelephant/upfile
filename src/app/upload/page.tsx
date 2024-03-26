1880bf6d1d2049aa1c276c0c78dd49387dae4d18
yzm <13884935@qq.com>
2024-03-26 23:03:53 +0900

    3

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -4,6 +4,7 @@ import { Input } from "@/components/ui/input"
 import { useRef, useState } from 'react';
 import styles from '.././page.module.css';
 import { Button } from "@/components/ui/button"
+import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
 const stats = [
   { id: 1, name: 'Transactions every 24 hours', value: '44 million' },
@@ -61,6 +62,33 @@ export default function page() {
       </div> */}
       {/* 页面第一部分 结束 */}
       {/* 页面第二部分 开始 */}
+
+      <div className="col-span-full">
+              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+                Cover file
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div className="text-center">
+                  <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    >
+                      <span>Upload a file</span>
+                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                    </label>
+                    <p className="pl-1">or drag and drop</p>
+                  </div>
+                  {/* <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p> */}
+                </div>
+              </div>
+            </div>
+        
+       
+       
+
+
       <form onSubmit={submit}>
         <input
           type="file"
